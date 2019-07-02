85890731bc503064ca90b7c9988f90158872ac1e
Alberto GT <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-07-02 09:00:32 +0200

    database cleaner

---
+++ bin/storj-cleaner.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const Config = require('../lib/monitor/config');
+const Cleaner = require('../lib/cleaner');
+
+program.version(require('../package').version);
+program.option('-c, --config <path_to_config_file>', 'path to the config file');
+program.parse(process.argv);
+
+var config = new Config(program.config);
+var cleaner = new Cleaner(config);
+
+cleaner.start(function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
+
+module.exports = cleaner;
