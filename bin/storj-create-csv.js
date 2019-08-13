7ac3484b2b8366c12026a039ba285408f546b31f
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-08-13 13:14:23 +0200

    csv creator for audit & monitor check again rejoined nodes

---
+++ bin/storj-create-csv.js
@@ -0,0 +1,25 @@
+const Storage = require('storj-service-storage-models');
+const Config = require('../lib/config');
+const program = require('commander');
+const logger = require('../lib/logger');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+program
+  .version('0.0.1')
+  .option('-c, --config <path_to_config_file>', 'path to the config file')
+  .parse(process.argv);
+
+const config = new Config(process.env.NODE_ENV || 'develop', program.config,
+    program.datadir);
+const { mongoUrl, mongoOpts } = config.storage;
+const storage = new Storage(mongoUrl, mongoOpts, { logger });
+
+storage.models.Contact.distinct("_id", (err, contacts) => {
+    const filename = path.join(__dirname, 'contacts.csv');
+    contacts = Array.from(contacts, c => [c]);
+    contacts = ['nodeID', ...contacts];
+    fs.writeFileSync(filename, contacts.join(os.EOL));
+    storage.connection.close();
+});
