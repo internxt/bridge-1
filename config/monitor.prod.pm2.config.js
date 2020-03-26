3203d7c0084d6203f28874a966d22a070f70d63a
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-03-26 12:27:47 +0100

    monitor replicate shards

--- config/monitor.prod.pm2.config.js
+++ config/monitor.prod.pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: 'bridge-monitor',
         script: 'storj-monitor',
-        args: '-- -c /root/.storj-bridge/config/production',
+        args: '-c /root/.storj-bridge/config/production',
         env: {
             STORJ_NETWORK: 'INXT',
             STORJ_BRIDGE: 'https://api.internxt.com',
