22354766709d5f8f28428e6132e2f9597483d154
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-07-01 10:12:19 +0000

    audit renewal pm2

---
+++ config/renewal.prod.pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: 'bridge-renewal',
+        script: './bin/storj-contract-renew.js',
+        cwd: '/root/bridge',
+        args: '-c /root/.storj-bridge/config/production',
+        env: {
+            STORJ_NETWORK: 'INXT',
+            STORJ_BRIDGE: 'https://api.internxt.com',
+            NODE_ENV: 'production'
+        }
+    }]
+}
+
