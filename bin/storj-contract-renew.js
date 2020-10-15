96717f1a299d3799949bd292d3a6a6bfaf9a0293
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-10-15 07:39:54 +0000

    contract renewal refactor and fixes

--- bin/storj-contract-renew.js
+++ bin/storj-contract-renew.js
@@ -6,6 +6,7 @@ const program = require('commander');
 const Config = require('../lib/config');
 const Renewal = require('../lib/renewal');
 const logger = require('../lib/logger');
+const _ = require('lodash')
 
 program.version(require('../package').version);
 program.option('-c, --config <path_to_config_file>', 'path to the config file');
@@ -21,28 +22,45 @@ const renewal = new Renewal(config);
 // Initialize database and network bindings.
 renewal.init();
 
-const counter = { processed: 0, renewed: 0, errored: 0 }
+// Show progression data log
+const counter = { processed: 0, renewed: 0, errored: 0, total: 0 }
+const log = setInterval(() => {
+  logger.info('Contracts proccesed: %s, renewed: %s, errored: %s, total: %s, left: %s', counter.processed, counter.renewed, counter.errored, counter.total, counter.total - counter.processed);
+}, 15000)
 
-setTimeout(() => {
-  renewal.on('counter-processed', () => { counter.processed++ });
-  renewal.on('counter-errored', () => { counter.errored++ });
-  renewal.on('counter-renewed', ({ contact, contract }) => { logger.info('Renewed %s %s', contact.nodeID, contract.data_hash); counter.renewed++; });
-  renewal.on('counter-errors', ({ contact, contract, error }) => logger.error('Error nodeID: %s, hash: %s, reason: %s', contact.nodeID, contract.data_hash, error.message))
 
-  const log = setInterval(() => {
-    logger.info('Contracts proccesed: %s, renewed: %s, errored: %s', counter.processed, counter.renewed, counter.errored);
-  }, 15000)
-
-  function startRenewal() {
-
-    renewal.initContractRenew((err) => {
-      console.log('Contract renew finished')
-    });  
+// Capture events
+renewal.on('counter-querysize', (size) => { counter.total = size });
+renewal.on('counter-processed', () => { counter.processed++ });
+renewal.on('counter-errored', () => { counter.errored++ });
+renewal.on('counter-renewed', ({ contact, contract }) => { logger.info('Renewed %s %s', contact.nodeID, contract.data_hash); counter.renewed++; });
+renewal.on('counter-errors', ({ contact, contract, error }) => {
+  logger.error('Error nodeID: %s, hash: %s, reason: %s', contact.nodeID, contract.data_hash, error.message)
+  switch (error.message) {
+    case 'Invalid farmer contract':
+      // TODO: Should check shard issues and clean database if needed
+      break;
+    default:
+      if (error.message.includes('timed out')) {
+        // console.log('TIMEOUT')
+      } else {
+        console.log('Not handled: %s', error.message)
+      }
   }
+})
 
-  renewal.on('end', () => {
-    startRenewal();
-  })
-
+renewal.on('end', () => {
   startRenewal();
-}, 3000);
+})
+
+function startRenewal() {
+  renewal.initContractRenew((err) => {
+    if (err) {
+      console.log('Contract renew round finished with errors: %s', err.message)
+    } else {
+      console.log('Contract renew round finished')
+    }
+  });
+}
+
+startRenewal();
