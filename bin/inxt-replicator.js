ce76696477249dbd538b7e31ebc64fb77c9f812d
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-10-28 18:10:10 +0000

    Replicator: first draft

---
+++ bin/inxt-replicator.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const Config = require('../lib/replicator/config');
+const Replicator = require('../lib/replicator');
+
+program.version(require('../package').version);
+program.option('-c, --config <path_to_config_file>', 'path to the config file');
+program.parse(process.argv);
+
+var config = new Config(program.config);
+var replicator = new Replicator(config);
+
+replicator.start(function (err) {
+  if (err) {
+    console.error(err)
+  }
+});
+
+module.exports = replicator;
